cebaf0a8125b5174b732a710ab2a985f47295c18
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2021-07-04 04:07:23 -0600

    Add blocks

--- main/java/com/extrabigsucc/PageControl/Init/InitBlocks.java
+++ main/java/com/extrabigsucc/PageControl/Init/InitBlocks.java
@@ -1,8 +1,15 @@
 package com.extrabigsucc.PageControl.Init;
 
 import com.extrabigsucc.PageControl.Blocks.Base.BaseBlock;
+import com.extrabigsucc.PageControl.Blocks.Base.BaseTileEntity;
+import com.extrabigsucc.PageControl.Blocks.OnlineDetectorBlock;
+import com.extrabigsucc.PageControl.Blocks.TeTestBlock;
 import com.extrabigsucc.PageControl.Blocks.TestBlock;
+import com.extrabigsucc.PageControl.Blocks.Tile.TeTestTileEntity;
 
 public class InitBlocks {
     public static final BaseBlock testBlock = new TestBlock("testBlock");
+    public static final BaseBlock teTestBlock = new TeTestBlock("teTestBlock");
+
+    public static final BaseBlock onlineDetectorBlock = new OnlineDetectorBlock("onlineDetectorBlock");
 }
