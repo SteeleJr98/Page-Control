ae18935257e124f245971a7478b32043dfbb19d7
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2021-07-04 04:07:45 -0600

    Added blocks

--- main/java/com/extrabigsucc/PageControl/Register/RegisterBlocks.java
+++ main/java/com/extrabigsucc/PageControl/Register/RegisterBlocks.java
@@ -1,11 +1,22 @@
 package com.extrabigsucc.PageControl.Register;
 
 import com.extrabigsucc.PageControl.Blocks.Base.BaseItemBlock;
+
+import com.extrabigsucc.PageControl.Blocks.Tile.TeTestTileEntity;
 import com.extrabigsucc.PageControl.Init.InitBlocks;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class RegisterBlocks {
     public static void Blocks() {
         GameRegistry.registerBlock(InitBlocks.testBlock, BaseItemBlock.class, "testBlock"); //No Texture
+        GameRegistry.registerBlock(InitBlocks.teTestBlock, BaseItemBlock.class, "teTestBlock"); //No Texture
+
+        GameRegistry.registerBlock(InitBlocks.onlineDetectorBlock, BaseItemBlock.class, "onlineDetectorBlock"); //No Texture
+
+    }
+
+    public static void TileEntities() {
+        GameRegistry.registerTileEntity(TeTestTileEntity.class, "TestTileEntity");
     }
 }
