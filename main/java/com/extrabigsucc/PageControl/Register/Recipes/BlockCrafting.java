1650efb8f278ecc04566e2ace9ecab1982beaf7c
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2021-07-07 03:12:19 -0600

    Added Recipe

--- main/java/com/extrabigsucc/PageControl/Register/Recipes/BlockCrafting.java
+++ main/java/com/extrabigsucc/PageControl/Register/Recipes/BlockCrafting.java
@@ -12,6 +12,7 @@ public class BlockCrafting {
 
     public static void Shaped() {
         GameRegistry.addShapedRecipe(new ItemStack(InitBlocks.onlineDetectorBlock), "IRI", "RCR", "IEI", 'I', Items.iron_ingot, 'R', Items.redstone, 'C', Blocks.crafting_table, 'E', Items.ender_pearl);
+        GameRegistry.addShapedRecipe(new ItemStack(InitBlocks.itemVoidBlock), "IRI", "RBR", "IEI", 'I', Items.iron_ingot, 'R', Items.redstone, 'B', Items.lava_bucket, 'E', Items.ender_pearl);
     }
 
     public static void Shapeless() {
