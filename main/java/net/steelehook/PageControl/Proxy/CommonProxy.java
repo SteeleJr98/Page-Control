48f04b5a04fbb8580d2cd2a13df769f2e430cb19
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2022-03-17 05:50:37 -0600

    Initial Commit
    Base Mod. heck making a new branch in the desktop app >:v
    did it anyway tho

--- main/java/net/steelehook/PageControl/Proxy/CommonProxy.java
+++
@@ -1,4 +0,0 @@
-package net.steelehook.PageControl.Proxy;
-
-public abstract class CommonProxy implements IProxy {
-}
--- main/java/net/steelehook/PageControl/Proxy/IProxy.java
+++
@@ -1,4 +0,0 @@
-package net.steelehook.PageControl.Proxy;
-
-public interface IProxy {
-}
