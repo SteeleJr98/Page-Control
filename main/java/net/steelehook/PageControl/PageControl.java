c5b0a8a628064b0dedaa7a3d13e7847f2fda4c51
Steele <54185181+SteeleJr98@users.noreply.github.com>
2022-07-11 21:32:15 -0600

    Update imports and deps

--- main/java/net/steelehook/PageControl/PageControl.java
+++ main/java/net/steelehook/PageControl/PageControl.java
@@ -4,6 +4,9 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.handshake.FMLHandshakeMessage.ModList;
+import cpw.mods.fml.relauncher.ModListHelper;
+import net.minecraftforge.common.ForgeModContainer;
 import net.steelehook.PageControl.Handlers.ConfigHandler;
 import net.steelehook.PageControl.Proxy.IProxy;
 import net.steelehook.PageControl.Register.RegisterBlocks;
@@ -19,13 +22,15 @@ public class PageControl
     public static final String NAME = "Page Control";
     public static final String MODID = "stpc170";
     //public static final String VERSION = "1.0.3"; //version taken from mcmod.info
-    public static final String DEPS = "required-after:Forge@[10.13.4.1614,)";
+    public static final String DEPS = "required-after:Forge@[10.13.4.1614,);required-after:stcore170";
 
     public static final String CLIENT_PROXY_CLASS = "net.steelehook.PageControl.Proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.steelehook.PageControl.Proxy.ServerProxy";
 
     @Mod.Instance(PageControl.MODID)
     public static PageControl instance = new PageControl();
+    
+    
 
     @SidedProxy(clientSide = PageControl.CLIENT_PROXY_CLASS, serverSide = PageControl.SERVER_PROXY_CLASS)
     public static IProxy proxy;
