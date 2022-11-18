d1ca0c954d2425405390e2a7d874825fab70b8d9
Steele <54185181+SteeleJr98@users.noreply.github.com>
2022-11-18 12:56:24 -0600

    Item Spawn Handling Almost Done
    it still crashes tho...

--- main/java/net/steelehook/PageControl/Handlers/SpawnStopHandling.java
+++ main/java/net/steelehook/PageControl/Handlers/SpawnStopHandling.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import scala.tools.nsc.interactive.CompilerControl.AskToDoFirstItem;
 
 public class SpawnStopHandling {
 	
@@ -18,7 +20,26 @@ public class SpawnStopHandling {
 		if (event.entity instanceof EntityItem) {
 			if (!event.world.isRemote) {
 				EntityItem item = (EntityItem) event.entity;
-				ServerLogging.sendMessageFromServer(item.getEntityItem().toString());
+				//ServerLogging.sendMessageFromServer(item.getEntityItem().getItem().getUnlocalizedNameInefficiently(item.getEntityItem()));
+				//ServerLogging.sendMessageFromServer(item.getEntityItem().getItem().getUnlocalizedName());
+				//ServerLogging.sendMessageFromServer(item.getEntityItem().getDisplayName());
+				//-->ServerLogging.sendMessageFromServer(item.getEntityItem().getUnlocalizedName());
+				//ServerLogging.sendMessageFromServer(item.getEntityItem().getItem().getItemStackDisplayName(item.getEntityItem()));
+				
+				String itemNameString = item.getEntityItem().getUnlocalizedName();
+
+				if (itemNameString.contains(":")) {
+					String[] nameList = itemNameString.split(":");
+					ServerLogging.sendMessageFromServer(nameList[1]);
+					for (String s : blackList) {
+						if (s.equals(nameList[1])) {
+							event.setCanceled(true);
+							ServerLogging.sendMessageFromServer("Event cancelled");
+						}
+					}
+				}
+				
+				
 				
 			}
 		}
